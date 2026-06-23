a1d0df561ed0c35bf0e803d327561264852934df
maneesh8123@gmail.com <maneesh8123@gmail.com>
2026-06-23 12:36:05 +0530

    identity column insertion fix with incremental loading

--- mssql_migration/models/asset_tracking/rfid_data.sql
+++ mssql_migration/models/asset_tracking/rfid_data.sql
@@ -1,6 +1,35 @@
-{{ config(materialized='table') }}
+{% set sql_header %}
+SET IDENTITY_INSERT [fa9710018].[asset_tracking].[rfid_data] ON;
+{% endset %}
+
+
+    {#
+        merge_exclude_columns=['ID'] prevents ID updates
+        sql_header enables IDENTITY_INSERT ON before merge
+        post_hook disables IDENTITY_INSERT OFF after merge
+     #}
+
+{{ 
+    config(
+    materialized='incremental',
+    unique_key='ID',
+    merge_exclude_columns=['ID'],
+    sql_header=sql_header,
+    post_hook="SET IDENTITY_INSERT [fa9710018].[asset_tracking].[rfid_data] OFF;"
+) }}
 
 
 SELECT
-    *
+    ID,
+    ScannedTime,
+    EpcHex,
+    AntennaPort,
+    AntennaName
 FROM {{ source('dbo','tbl_rfid_data') }}
+
+{% if is_incremental() %}
+    WHERE ID > (
+        SELECT COALESCE(MAX(ID), 0)
+        FROM {{ this }}
+    )
+{% endif %}
